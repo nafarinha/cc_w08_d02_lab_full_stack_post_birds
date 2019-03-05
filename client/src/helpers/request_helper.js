41f69037f38d37ce84963622c7c7b1016c605ac2
Nuno Farinha <nadfarinha@gmail.com>
2019-03-05 13:25:24 +0000

    Add post function to request helper

--- client/src/helpers/request_helper.js
+++ client/src/helpers/request_helper.js
@@ -7,6 +7,16 @@ RequestHelper.prototype.get = function () {
     .then((response) => response.json());
 };
 
+RequesHelper.prototype.post = function (payload) {
+  return fetch(this.url, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then( (response) => response.json() );
+}
+
+
 RequestHelper.prototype.delete = function (id) {
   return fetch(`${this.url}/${id}`, {
     method: 'DELETE'
